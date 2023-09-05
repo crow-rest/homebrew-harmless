ca601a4343f8db60cc3e7b5d77403d6716fdd87c
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-09-05 00:43:31 -0400

    zig2

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -10,17 +10,6 @@ class Zig < Formula
     regex(/href=.*?zig[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-#   bottle do
-#     rebuild 2
-#     sha256 cellar: :any,                 arm64_ventura:  "f9fba5a3f6ce5a3005d6f5c91f06a2630237747eb98bbf7be39b885856f7a064"
-#     sha256 cellar: :any,                 arm64_monterey: "5bb3325fbf88ed5d129b289bb53d7d56184bd89708398521bf5ae5275ba4fde8"
-#     sha256 cellar: :any,                 arm64_big_sur:  "e148c22748a77f420327bdc267ad8dcc1fcaa4cdd307fba435693ce02d9242f9"
-#     sha256 cellar: :any,                 ventura:        "1e7f133b829d858de19d5bc60a581ebccae19fcb2cf4b7dfbef36cae27705aa6"
-#     sha256 cellar: :any,                 monterey:       "d89854a3bdc7bfefaa695c6369ee708151fe3f0ec70dc38b30c92c981b6f58c0"
-#     sha256 cellar: :any,                 big_sur:        "eb4a6e0dd5714db1d682ac049eed6e92993badc9d5a9e9d593248820c5ca0070"
-#     sha256 cellar: :any_skip_relocation, x86_64_linux:   "eefd8e8b1a71b27ce3b5a9c9c3ea8bcb4cf0f74221035d5da84b11d31fa33569"
-#   end
-
   depends_on "cmake" => :build
   depends_on "llvm@16" => :build
   depends_on "zstd"
