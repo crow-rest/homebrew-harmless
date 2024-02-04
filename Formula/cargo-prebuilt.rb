a2f588d87e1c7c5eff6c2750b573390a7786c2a3
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2024-02-04 03:12:06 -0500

    cargo-prebuilt: update to 0.6.6

--- Formula/cargo-prebuilt.rb
+++ Formula/cargo-prebuilt.rb
@@ -1,7 +1,7 @@
 class CargoPrebuilt < Formula
   desc "Cli tool to download prebuilt binaries of some crate.io crates"
   homepage "https://github.com/cargo-prebuilt/cargo-prebuilt"
-  version "0.6.5"
+  version "0.6.6"
   license "MIT"
   head "https://github.com/cargo-prebuilt/cargo-prebuilt.git", branch: "main"
 
@@ -10,32 +10,25 @@ class CargoPrebuilt < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    root_url "https://github.com/crow-rest/homebrew-harmless/releases/download/cargo-prebuilt-0.6.5"
-    sha256 cellar: :any_skip_relocation, ventura:      "c26afc1c2ee7a681d3f035c65fb2dd38e4731821d143deda1c31e3186cb307d2"
-    sha256 cellar: :any_skip_relocation, monterey:     "c672194caff940a1a2656d732870f8d492869327684ba3c569b79b2dd3f3739d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9c852f045822fb29e509f7d9c50efa9e7c5f0f34158676e0f6887291b8f2f0c6"
-  end
-
   on_macos do
     on_intel do
       url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/x86_64-apple-darwin.tar.gz"
-      sha256 "bbfbd50f1cfe09cbec2ca2a09b5ec72815fd40dc75d98161652caf00ad81098e"
+      sha256 "1bebe40675b263e79e6866140db54652a4ac84e138c9903dac5d7c45bbe4cecd"
     end
     on_arm do
       url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/aarch64-apple-darwin.tar.gz"
-      sha256 "f42fa7b85feeca03c0c15f1ff03a11568681d8862b9618da122a4417308ec91e"
+      sha256 "fe7900e821b2b681447180b7c7ebc2e69bfe98d55b24c2b9d52905d65b570307"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2adcdefb761af5e910cb21bc716753ffa93675b4d2cc429dcfafe3e66aa22a90"
+      sha256 "554e845d8131ed9efec377dc8df4cedc0485b8bc029febefe988cfb79d5a38b5"
     end
     on_arm do
       url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e991765de2643faca650553cb5bac0716b22bc084840d5ebbbf243341d308277"
+      sha256 "f28f1475c29c2d9810cccace4569e143626a0e635283cccfd764c3beb3dcfafb"
     end
   end
 
