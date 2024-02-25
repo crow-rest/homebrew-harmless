e3bb534f53ad84d628d078bbfd4ce2e41592d796
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2024-02-25 03:56:54 -0500

    Remove versions
    sigh

--- Formula/cargo-prebuilt.rb
+++ Formula/cargo-prebuilt.rb
@@ -1,7 +1,6 @@
 class CargoPrebuilt < Formula
   desc "Cli tool to download prebuilt binaries of some crate.io crates"
   homepage "https://github.com/cargo-prebuilt/cargo-prebuilt"
-  version "0.6.6"
   license "MIT"
   head "https://github.com/cargo-prebuilt/cargo-prebuilt.git", branch: "main"
 
@@ -19,22 +18,22 @@ class CargoPrebuilt < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/x86_64-apple-darwin.tar.gz"
+      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v0.6.6/x86_64-apple-darwin.tar.gz"
       sha256 "1bebe40675b263e79e6866140db54652a4ac84e138c9903dac5d7c45bbe4cecd"
     end
     on_arm do
-      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/aarch64-apple-darwin.tar.gz"
+      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v0.6.6/aarch64-apple-darwin.tar.gz"
       sha256 "fe7900e821b2b681447180b7c7ebc2e69bfe98d55b24c2b9d52905d65b570307"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v0.6.6/x86_64-unknown-linux-gnu.tar.gz"
       sha256 "554e845d8131ed9efec377dc8df4cedc0485b8bc029febefe988cfb79d5a38b5"
     end
     on_arm do
-      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v#{version}/aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/download/v0.6.6/aarch64-unknown-linux-gnu.tar.gz"
       sha256 "f28f1475c29c2d9810cccace4569e143626a0e635283cccfd764c3beb3dcfafb"
     end
   end
